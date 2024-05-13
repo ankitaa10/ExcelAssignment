50db4fb37b9d296cd87f30ddccfa5ffc8d699220
ankitaa10 <ankitaa1006@gmail.com>
2024-05-13 17:33:45 +0530

    Second Commit

--- src/test/java/ApachePOI/Assignment/ExcelDataProcessor.java
+++ src/test/java/ApachePOI/Assignment/ExcelDataProcessor.java
@@ -7,20 +7,23 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
 public class ExcelDataProcessor {
-    @Test
-    @Parameters({"namesA","namesB"})
+//    @Test
+//    @Parameters({"namesA","namesB"})
 	public static void DataProcessor(String namesA,String namesB ) throws IOException {
 
-
+//    	<parameter name="namesA" value="Ankitaa,Anirudh,Anavi,Haru" />
+//        <parameter name="namesB" value="Anvith,Ankith,Yamini,Haru" />
     	
     	String[] namesinACol = namesA.split(",");
     	String[] namesinBCol = namesB.split(",");
@@ -74,11 +77,11 @@ public class ExcelDataProcessor {
 
 
 
+@DataProvider(name="excelData")
+    public static String[][] readExcel() throws IOException {
 
-    public static String[][] readExcel(String inputPath) throws IOException {
 
-
-    	     File file = new File(inputPath);
+    	     File file = new File(System.getProperty("user.dir")+"/Utils/AssignmentReadData.xlsx");
              FileInputStream fis = new FileInputStream(file);
 
           try {
@@ -125,7 +128,7 @@ public class ExcelDataProcessor {
     }
 
 
-    public static void writeExcel(String outputPath, Set<String> uniqueNames, Set<String> uniqueLocations, Set<String> duplicates) throws IOException {
+    public static void writeExcel(String outputPath, Set<String> uniqueNamesA, Set<String> uniqueNamesB, Set<String> duplicates) throws IOException {
 
 
         try {
@@ -140,21 +143,21 @@ public class ExcelDataProcessor {
             headerRow.createCell(1).setCellValue("Unique Names-B");
             headerRow.createCell(2).setCellValue("Duplicates Names- A & B");
 
-            //  to get unique names and locations
+            
            rowIndex = 1;
-            for (String name : uniqueNames) {
+            for (String nameA : uniqueNamesA) {
                 Row row = sheet.createRow(rowIndex++);
-                row.createCell(0).setCellValue(name);
+                row.createCell(0).setCellValue(nameA);
 
 
             }
 
 
             rowIndex = 1;
-            for (String location : uniqueLocations) {
+            for (String nameB : uniqueNamesB) {
                 Row row = sheet.getRow(rowIndex++);
                 if (row == null) row = sheet.createRow(rowIndex - 1);
-                row.createCell(1).setCellValue(location);
+                row.createCell(1).setCellValue(nameB);
 
             }
 
@@ -179,10 +182,88 @@ public class ExcelDataProcessor {
              }
     }
 
+    
+    
+    public static String[][] getExcelData() throws IOException {
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/Utils/AssignmentReadData.xlsx");
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheet("Sheet1");
+
+        int rowCount = sheet.getLastRowNum() + 1;
+        int columnCount = sheet.getRow(0).getLastCellNum();
+
+        String[][] data = new String[rowCount][columnCount];
+
+        for (int i = 0; i < rowCount; i++) {
+            Row row = sheet.getRow(i);
+            for (int j = 0; j < columnCount; j++) {
+                Cell cell = row.getCell(j);
+                data[i][j] = cell.getStringCellValue();
+            }
+        }
+
+        workbook.close();
+        fis.close();
+
+        return data;
+    }
+   
+    
+    @Test(dataProvider="excelData")
+   public static void DataProcessor2(String[][] readExcelData ) throws IOException {
+    	
+    	String[] namesinACol = readExcelData[0];
+    	String[] namesinBCol = readExcelData[1];
+      Set<String> uniqueNamesA =  new HashSet<String>();
+      Set<String> uniqueNamesB =  new HashSet<String>();
+      Set<String> duplicateNames =  new HashSet<String>();
+      boolean duplicateValue;
+      // logic
+   
+      for (String nameA : namesinACol) {
+    	  duplicateValue = false;
+    	  for (String nameB: namesinBCol) {
+
+    		   if(nameA.equals(nameB)){
+
+    			   duplicateNames.add(nameA);
+    			   duplicateValue = true;
+
+    		   }
+    		  
+    		   
+    	  }
+    	  if(!duplicateValue) {
+			   uniqueNamesA.add(nameA);
+		   }
+
+
+    }
+      
+      for(String nameB : namesinBCol) {
+    	  duplicateValue = false;
+    	  for(String Value : duplicateNames) {
+    		  
+    		  if(nameB.equals(Value)) {
+    			  duplicateValue = true;    			  
+    		  }
+    		  
+    		   
+    	  }
+    	  if(!duplicateValue) {
+    		  uniqueNamesB.add(nameB);
+		   }
+    	  
+      }
 
 
 
- }
+         writeExcel(System.getProperty("user.dir")+"/Utils/AssignmentWriteData.xlsx", uniqueNamesA, uniqueNamesB, duplicateNames);
+    }
+
+    }
+
+ 
 
 
 
